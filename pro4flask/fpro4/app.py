fab88a65214732c1f27a42e7d8c027ee8672279d
dhkim <shoeswearing@naver.com>
2026-03-05 16:34:48 +0900

    Flask(쿠키, 세션)

--- pro4flask/fpro4/app.py
+++ pro4flask/fpro4/app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/condition")
+def condition():
+    score = 85
+    return render_template("condition.html", score = score)
+
+@app.route("/loop")
+def loop():
+    users = ["손오공","사오정","저팔계"]
+    return render_template("loop.html", users = users)
+
+@app.route("/filter")
+def filter():
+    message = "hello flask jinja2"
+    price = 12345
+    return render_template("filter.html", message = message, price = price)
+
 if __name__ == '__main__':
     app.run(debug=True)
